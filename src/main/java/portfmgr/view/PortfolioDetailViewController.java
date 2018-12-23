7daa372ae3f9550b3e4eb69e8ad1cfcc4281f27d
Marc Steiner <spare@gmx.ch>
2018-12-23 10:20:32 +0100

    Initialzed branch and startet with first code

--- src/main/java/portfmgr/view/PortfolioDetailViewController.java
+++ src/main/java/portfmgr/view/PortfolioDetailViewController.java
@@ -1,15 +1,31 @@
 package portfmgr.view;
 
 import javafx.fxml.FXML;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import portfmgr.portfmgrApplication;
+import portfmgr.model.Portfolio;
+/**
+ * This class represents one of the main controllers and manage the detail view about the portfolios.
+ * 
+ * @author Marc Steiner
+ */
 
 public class PortfolioDetailViewController {
 
-	// Reference to the main app (portfmgrApplication)
 	private portfmgrApplication mainApp;
+	private Portfolio portfolio;
+	private String portfolioName;
+	private String portfolioCurrency;
+	
+	public PortfolioDetailViewController(Portfolio portfolio) {
+		this.portfolio = portfolio;
+		portfolioName = portfolio.getPortfolioName();
+		portfolioCurrency = portfolio.getPortfolioCurrency();
+		updatePortfolio();
+	}
 	
 	@FXML
 	private Button openDashboard;
@@ -45,6 +61,29 @@ public class PortfolioDetailViewController {
 	public void setMainApp(portfmgrApplication mainApp) {
 		this.mainApp = mainApp;
 
+	}
+	
+
+	public void updatePortfolio() {
+		editPortfolio(portfolioName, portfolioCurrency);
+		// neue INstanz von OnlineCourseQuery, welche ein JSON Objekt zurück gibt
+		/*
+		 * TO DO
+		 * claculte everything and update view
+		 */
+		
+	}
+	
+
+	public void editPortfolio(String portfolioName, String portfolioCurrency) {
+		portfolio.setPortfolioCurrency(portfolioCurrency);
+		portfolio.setPortfolioName(portfolioName);
+		
+		/*
+		 * TO DO
+		 * Call Portfolio methods and set name and currency
+		 */
+		
 	}
 
 }
--- src/main/java/portfmgr/view/PortfolioViewController.java
+++ src/main/java/portfmgr/view/PortfolioViewController.java
@@ -64,6 +64,7 @@ public class PortfolioViewController {
 			// Gives the controller class access to the mainApp in order to set the scene within the rootLayout.
 			PortfolioDetailViewController controller = loader.getController();
 			controller.setMainApp(mainApp);
+		
 
 			// Opens the portfolioView within the rootLayout
 			mainApp.getRootLayout().setCenter(overview);
