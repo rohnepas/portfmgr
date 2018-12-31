47aa399f37e00dbf0ecfd218a808ff7356c8a0d9
Marc Steiner <spare@gmx.ch>
2018-12-31 11:52:06 +0100

    Working and improved UpdateViewController. Button functions still not working properly

--- src/main/java/portfmgr/view/PortfolioUpdateViewController.java
+++ src/main/java/portfmgr/view/PortfolioUpdateViewController.java
@@ -13,7 +13,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -21,11 +20,17 @@ import portfmgr.portfmgrApplication;
 import portfmgr.model.Portfolio;
 import portfmgr.model.PortfolioRepository;
 
+/**
+ * This class represents the controller for the update view which is used to change name and currency of the portfolio.
+ * 
+ * @author Marc Steiner
+ */
+
 @Controller
 public class PortfolioUpdateViewController implements Initializable {
 	
 	private portfmgrApplication mainApp;
-	private String newPortfolioName;
+	private String portfolioName;
 	private String portfolioCurrency;
 	private Portfolio portfolio;
 	ObservableList<String> currencyList = FXCollections.observableArrayList("CHF", "EUR", "USD");
@@ -40,7 +45,7 @@ public class PortfolioUpdateViewController implements Initializable {
 	@FXML
 	private ComboBox<String> currencyBox;
 	@FXML
-	private TextField portfolioName;
+	private TextField newPortfolioName;
 	
 	private Stage dialogStage;
 	
@@ -54,62 +59,65 @@ public class PortfolioUpdateViewController implements Initializable {
     }
     
     /**
-     * Called when the user clicks submit.
+     * Called when the user clicks submit. It writs the new portfolio name and currency direct into the database
      */
     @FXML
     private void handleSubmit() {
-    	System.out.println("SUBMIT");
-    	/*
+    	
         if (isInputValid()) {
-        	portfolio.setPortfolioName(portfolioName.getText()); 
+        	System.out.println(newPortfolioName.getText());
+        	System.out.println(currencyBox.getValue());
+        	/*
+        	 * Folgneder Code kann erst implementiert werden, wenn ein Portfolio vorhanden ist. sonst Exception
+        	 * 
+        	portfolio.setPortfolioName(newPortfolioName.getText()); 
         	portfolio.setPortfolioCurrency(currency.getValue());
+        	portRepo.findById(portfolio.getId()). UPDATE???????;
         	
             dialogStage.close();
-        
-           // portRepo.findById(portfolio.getId()). UPDATE???????;
+         */
         }
-        */
+       
+    }
+    
+    private boolean isInputValid() {
+    	
+    	if (newPortfolioName.getText() == null || newPortfolioName.getText().trim().isEmpty()) {
+ 
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Eingabefehler");
+            alert.setHeaderText("Portfolioname darf nicht leer sein");
+            alert.showAndWait();
+            
+    		return false;
+    	}
+    	
+    	return true;
     }
     
     public void setPortfolio (Portfolio portfolio) {
-    	//this.portfolio = portfolio;
-    	//portfolio.getPortfolioName();
-    	//portfolio.getPortfolioCurrency();
-    	// Aktueller Werte von Name und Währung schon anzeigen in Pop-Ip
+    	/*
+    	 * Zuerst muss Portoflio richtig implementiert werden. Bei der Auswahl des gewünschten Portfolios, muss
+    	 * das Portfolio an den PortfolioDetailViewController mittels setActualPortoflio() übergeben werden.
+    	 * Dieses Portfolio wird dann in ditPortfolio() im Code mainApp.openPortfolioUpdateView(portfolio); an den PortfolioUpdateViewController übergeben
+    	 * Erst dann kann folgender Code ausgepfhrt werden, da sonst eine NULL POINTEr EXCEPTION geworfen wird
+    	 * 
+    	 * this.portfolio = portfolio;
+    	 * portfolioName = portfolio.getPortfolioName();
+    	 * portfolioCurrency = portfolio.getPortfolioCurrency();
+    	 */
     	
     }
     
-    public String getNewPortfolioName() {
-    	return newPortfolioName;
+    public String getPortfolioName() {
+    	return portfolioName;
     }
     
     public String getPortfolioCurrency() {
     	return portfolioCurrency;
     }
     
-    private boolean isInputValid() {
-    	String errorMessage = "";
-  
-    	if (portfolioName.getText() == null || currencyBox.getValue() == null) {
-    		errorMessage = "Please provide a valid input";
-    		
-    	}
-    	
-    	if (errorMessage.length() == 0) {
-            return true;
-        } 
-    	else {
-            // Show the error message.
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.initOwner(dialogStage);
-            alert.setTitle("Invalid Fields");
-            alert.setHeaderText("Please correct invalid fields");
-            alert.setContentText(errorMessage);
-            
-            alert.showAndWait();
-            return false;
-        }
-    }
     
     public void setDialogStage(Stage dialogStage) {
         this.dialogStage = dialogStage;
@@ -117,13 +125,15 @@ public class PortfolioUpdateViewController implements Initializable {
     
 	public void setMainApp(portfmgrApplication mainApp) {
 		this.mainApp = mainApp;
-
 	}
     
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		currencyBox.setItems(currencyList);
+		
+		// folgende zwei Werte "TEST" sollen mit portfolioName und portfolioCurrency ersetzt werden
 		currencyBox.setValue("TEST");
+		newPortfolioName.setText("TEST");
 		
 	}
 
