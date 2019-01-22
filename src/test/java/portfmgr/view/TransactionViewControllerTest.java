890beb9f865bc93b7279809bfa19079e62655cd9
Pascal Rohner <pascal@prohner.com>
2019-01-22 21:04:33 +0100

    imporved input validation on transaction view.

--- src/test/java/portfmgr/view/TransactionViewControllerTest.java
+++ src/test/java/portfmgr/view/TransactionViewControllerTest.java
@@ -1,6 +1,7 @@
 package portfmgr.view;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -17,5 +18,35 @@ public class TransactionViewControllerTest {
 		TransactionViewController transViewController = new TransactionViewController();
 		Double result = transViewController.calculateTotal("Kauf", 1000.0, 2.0, 80.0);
 		assertEquals(2080.0, result, 0);
+	}
+	
+	/**
+	 * checks whether the the fieldInput is correctly validated. 
+	 * It should only be possible to enter numbers. The numbers can consist of one point and four decimal places.
+	 * Checks the False case 
+	 * 
+	 * @author Pascal Rohner
+	 */
+	@Test
+	public void validateFieldInputFalseTest() {
+		TransactionViewController transViewController = new TransactionViewController();
+		boolean result = transViewController.validateFieldInput("ad 12321");
+		assertTrue(result == false);
+		
+	}
+	
+	/**
+	 * checks whether the the fieldInput is correctly validated. 
+	 * It should only be possible to enter numbers. The numbers can consist of one point and four decimal places.
+	 * Checks the True case 
+	 * 
+	 * @author Pascal Rohner
+	 */
+	@Test
+	public void validateFieldInputTrueTest() {
+		TransactionViewController transViewController = new TransactionViewController();
+		boolean result = transViewController.validateFieldInput("1235.2358");
+		assertTrue(result == true);
+		
 	}
 }
